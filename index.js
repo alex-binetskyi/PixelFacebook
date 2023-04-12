e7628daddecebf795d1e6c95cf083615c6c05169
alex-softevol <ab@softevol.com>
2023-04-12 23:21:45 +0300

    sync

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 const web_link = "http://decoplayingcard.com";
 
 bot.start((ctx) =>
-  ctx.reply("Welcome to the Test Form.", {
+  ctx.reply("Welcome to the Test Fb Pixel.", {
     reply_markup: {
       keyboard: [[{ text: "TEST FORM", web_app: { url: web_link } }]],
     },
